ad416178af03b8743ff07c10708fe45a126cadaf
Coderdivine <chimdi4332@gmail.com>
2022-07-13 12:15:07 +0100

    update

--- index.js
+++ index.js
@@ -10,3 +10,42 @@ const enc = web3.eth.accounts.encrypt(account_1.privateKey, "password");
 console.log(enc)
 const enc_single_wallet = web3.eth.accounts.wallet.encrypt(wallet_1);
 console.log(enc_single_wallet)
+class DogWallet {
+    constructor(name,address,numberOfWallet){
+        this.name == name;
+        this.address == address;
+        this.numberOfWallet == numberOfWallet;
+    }
+    generateAddress(type){
+        if(type == "normal_account"){
+            const acc = web3.eth.accounts.create();
+            return acc;
+        }else if(type  == "user_wallet"){
+            const acc = web3.eth.accounts.wallet.create(numberOfWallet,"@@@")
+            return acc;
+        }else{
+            return "type can't be read :("
+        }     
+    }
+    hashWallet(type,password,privateKey){
+        if(type == "normal_account"){
+            const hash = web3.eth.accounts.encrypt(privateKey,password)
+            return hash
+        }else{
+           const hash = web3.eth.accounts.wallet.encrypt(privateKey,password)
+           return hash
+        }
+    }
+    de_hashWallet(type,password,hashed){
+        if(type == "normal_account"){
+            const hash = web3.eth.accounts.decrypt(hashed,password)
+            return hash
+        }else{
+           const hash = web3.eth.accounts.wallet.decrypt(hashed,password)
+           return hash
+        } 
+    }
+}
+const dogWallet = new DogWallet("Divine",account_1.address,10);
+const generateAddress = dogWallet.generateAddress("normal_account")
+console.log(generateAddress)
